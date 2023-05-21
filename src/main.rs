f3e9b09cf37cb2840045fbf11d62f28a38befeee
Peter Simonsson <peter.simonsson@gmail.com>
2023-05-21 09:19:27 +0200

    Use crossterm to format the output

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,13 @@ mod args;
 
 use anyhow::Result;
 use clap::Parser;
+use crossterm::{
+    cursor::{Hide, RestorePosition, SavePosition, Show},
+    execute, queue,
+    style::Print,
+};
 use futures::stream::StreamExt;
+use std::io::{stdout, Write};
 use tokio_serial::SerialPortBuilderExt;
 use tokio_util::codec::{Decoder, LinesCodec};
 
@@ -11,6 +17,10 @@ use crate::args::Args;
 #[tokio::main]
 async fn main() -> Result<()> {
     let args = Args::parse();
+    let mut stdout = stdout();
+
+    execute!(stdout, Hide)?;
+
     println!("Device: {}", args.device());
 
     let mut port = tokio_serial::new(args.device(), 115200).open_native_async()?;
@@ -44,16 +54,22 @@ async fn main() -> Result<()> {
             last_line = false;
 
             if lines.len() == 5 {
+                queue!(stdout, SavePosition)?;
                 for line in &lines {
                     if let Some((_, line)) = line.split_once(' ') {
-                        println!("{}", line);
+                        queue!(stdout, Print(format!("{}\n", line)))?;
                     }
                 }
+                queue!(stdout, RestorePosition)?;
+
+                stdout.flush()?;
             }
 
             lines.clear();
         }
     }
+
+    execute!(stdout, Show)?;
 
     Ok(())
 }
