7019b630cbb458e191732404c5cb7ecdd8c89b3c
Peter Simonsson <peter.simonsson@gmail.com>
2023-06-06 13:08:58 +0200

    Add chart for current value

--- src/app.rs
+++ src/app.rs
@@ -1,11 +1,14 @@
 use std::time::Duration;
 
 use anyhow::{anyhow, Result};
+use chrono::Local;
 use crossterm::event::{self, Event, KeyCode};
 use tui::{
     backend::Backend,
     layout::{Constraint, Direction, Layout},
-    widgets::{Block, Borders, Cell, Row, Table},
+    style::{Color, Style},
+    symbols,
+    widgets::{Axis, Block, Borders, Cell, Chart, Dataset, Row, Table},
     Frame, Terminal,
 };
 
@@ -13,7 +16,7 @@ use crate::gps::{Gps, Message};
 
 pub struct App {
     gps: Gps,
-    current: u64,
+    current: Vec<(f64, f64)>,
     devcurr: f64,
     devaccum: f64,
     dac1: Option<u32>,
@@ -25,7 +28,7 @@ impl App {
     pub fn new(device: &str) -> Self {
         App {
             gps: Gps::new(device.to_string()),
-            current: 0,
+            current: Vec::new(),
             devcurr: 0.0,
             devaccum: 0.0,
             dac1: None,
@@ -54,7 +57,7 @@ impl App {
     fn ui<B: Backend>(&self, f: &mut Frame<B>) {
         let chunks = Layout::default()
             .direction(Direction::Vertical)
-            .constraints([Constraint::Percentage(20), Constraint::Percentage(80)].as_ref())
+            .constraints([Constraint::Percentage(20), Constraint::Percentage(20)].as_ref())
             .split(f.size());
 
         let list = self.generate_data_list();
@@ -63,12 +66,31 @@ impl App {
             .block(Block::default().title("GPSDXO Data").borders(Borders::ALL));
 
         f.render_widget(table, chunks[0]);
+
+        let now = Local::now().timestamp() as f64;
+
+        let datasets = vec![Dataset::default()
+            .marker(symbols::Marker::Braille)
+            .style(Style::default().fg(Color::Cyan))
+            .data(&self.current)];
+
+        let chart = Chart::new(datasets)
+            .block(Block::default().title("Current").borders(Borders::ALL))
+            .x_axis(Axis::default().bounds([now - 300.0, now]))
+            .y_axis(Axis::default().bounds([79999995.0, 80000005.0]));
+
+        f.render_widget(chart, chunks[1]);
     }
 
     fn process_message(&mut self, message: &Message) -> Result<()> {
         match message {
             Message::Curr(data) => {
-                self.current = *data;
+                self.current
+                    .push((Local::now().timestamp() as f64, *data as f64));
+
+                if self.current.len() > 300 {
+                    self.current.remove(0);
+                }
             }
             Message::DevCurr(data) => {
                 self.devcurr = *data;
@@ -94,6 +116,10 @@ impl App {
     }
 
     fn generate_data_list(&self) -> Vec<Row> {
+        let current = match self.current.last() {
+            Some(data) => data.1.to_string(),
+            None => "".to_string(),
+        };
         let dac1 = match self.dac1 {
             Some(dac1) => dac1.to_string(),
             None => "".to_string(),
@@ -104,10 +130,7 @@ impl App {
         };
 
         vec![
-            Row::new(vec![
-                Cell::from("Current"),
-                Cell::from(self.current.to_string()),
-            ]),
+            Row::new(vec![Cell::from("Current"), Cell::from(current)]),
             Row::new(vec![
                 Cell::from("Deviation current"),
                 Cell::from(format!("{}Hz", self.devcurr)),
